095d11777c4aaf8a19d1f59e9faf91c0867a026c
brainstorm98 <143192190+brainstorm98@users.noreply.github.com>
2023-09-07 00:18:39 +0300

    Add files via upload

---
+++ Week_10_3.X/build/default/production/_ext/1213170632/LCD.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1213170632/LCD.p1:  \
+../Week_10_2.X/LCD.c 
---
+++ Week_10_3.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Week_10_3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Week_10_3.X.production.hex
